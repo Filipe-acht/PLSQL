1ce0cd4ee319de8ad90272201d8def866070efca
Filipe N Costa Soares <58039037+Filipe-acht@users.noreply.github.com>
2022-02-01 20:08:32 -0300

    Atualizando

---
+++ Aprendendo/padroes_de_codificacao_sugerido.sql
@@ -0,0 +1,18 @@
+-- Padrões de codificação sugeridos
+
+SET SERVEROUTPUT ON
+DECLARE
+  vBloco1 VARCHAR2(20) := 'Bloco 1';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Referenciando variável do Bloco 1: ' || vBloco1);
+  -- Se voce referencia vbloco2 aqui ocorrerá Erro
+  DECLARE
+    vBloco2 VARCHAR2(20) := 'Bloco 2';
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('Referenciando variável do Bloco 1: ' || vBloco1);
+    DBMS_OUTPUT.PUT_LINE('Referenciando variável do Bloco 2: ' || vBloco2);
+  END;
+  DBMS_OUTPUT.PUT_LINE('Referenciando variável do Bloco 1: ' || vBloco1);
+  -- Se voce referencia vbloco2 aqui ocorrerá Erro
+END;
+

88bf560a02ada03f5b90a3941c4d8afd532c1f75
filipesoares <filipebnks@gmail.com>
2022-03-02 22:58:45 -0300

    Usando Sys_refcursor

---
+++ Aprendendo/sys_refcursor.sql
@@ -0,0 +1,93 @@
+-- Utilizando SYS_REFCURSOR
+CREATE OR REPLACE PROCEDURE PRC_CURSOR_EMPLOYEES
+  (pemployees_cursor OUT SYS_REFCURSOR) /* retorna como saida um cursor */
+IS
+BEGIN
+
+  OPEN pemployees_cursor /* abrindo o cursor declarado no parametro */
+  /* result set do cursor */
+  FOR /* select do cursor */
+	SELECT first_name,
+           last_name 
+	FROM employees;
+    
+END PRC_CURSOR_EMPLOYEES;
+
+-- Procedure referenciando o Parametro OUT SYS_REFCURSOR
+
+CREATE OR REPLACE PROCEDURE PRC_DISPLAY_EMPOYEES
+IS
+  employees_cursor  SYS_REFCURSOR; /* se torna um cursor */
+  vfirst_name  employees.first_name%TYPE;
+  vlast_name   employees.last_name%TYPE;
+BEGIN
+  PRC_CURSOR_EMPLOYEES(employees_cursor); /* chamando a procedure e realizando o open do cursor */
+  
+  LOOP
+    FETCH  employees_cursor
+    INTO   vfirst_name,
+           vlast_name;
+    EXIT   
+        WHEN employees_cursor%NOTFOUND;
+	
+    DBMS_OUTPUT.PUT_LINE(vfirst_name || ' ' || vlast_name); /* imprimindo */
+
+  END LOOP;
+  
+  CLOSE employees_cursor;
+  
+END PRC_DISPLAY_EMPOYEES;
+
+-- Executando a Procedure PRC_DISPLAY_EMPOYEES
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+execute PRC_DISPLAY_EMPOYEES;
+
+-- Variável Cursor e Reference Cursor 
+
+CREATE OR REPLACE FUNCTION FNC_GET_EMPOYEES
+  (pemployee_id  IN NUMBER)
+  RETURN SYS_REFCURSOR
+IS
+  employees_cursor  SYS_REFCURSOR;
+BEGIN
+  OPEN employees_cursor  
+  FOR
+    SELECT first_name, last_name
+    FROM   employees
+    WHERE  employee_id = pemployee_id;
+    
+  RETURN employees_cursor;
+  
+END FNC_GET_EMPOYEES;
+
+-- Referenciando a Função
+
+CREATE OR REPLACE PROCEDURE PRC_DISPLAY_EMPOYEES2
+  (pemployee_id IN NUMBER)
+IS
+  employees_cursor  SYS_REFCURSOR;
+  vfirst_name  employees.first_name%TYPE;
+  vlast_name   employees.last_name%TYPE;
+BEGIN
+  employees_cursor := FNC_GET_EMPOYEES(pemployee_id);
+  
+  LOOP
+    FETCH  employees_cursor
+    INTO   vfirst_name, vlast_name;
+    EXIT WHEN employees_cursor%NOTFOUND;
+	
+    DBMS_OUTPUT.PUT_LINE(vfirst_name || ' ' || vlast_name);
+
+  END LOOP;
+  
+  CLOSE employees_cursor;
+END PRC_DISPLAY_EMPOYEES2;
+
+-- Executando a procedure 
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+execute PRC_DISPLAY_EMPOYEES2(100);
+

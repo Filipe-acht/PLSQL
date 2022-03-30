d9a314795ee716ad279769176d7f82f7b06696b9
Filipe Soares <58039037+Filipe-acht@users.noreply.github.com>
2022-03-30 10:50:05 -0300

    Create dbms_random_string.sql

---
+++ Aprendendo/dbms_random_string.sql
@@ -0,0 +1,10 @@
+
+SELECT DBMS_RANDOM.STRING('U', 1) FROM DUAL
+/*
+ * O 'L' no código se refere a lowercase (somente caracteres minúsculos). Você pode substituir por:
+ * O 'U' para somente caracteres maiúsculos;
+ * O 'A' para caracteres maiúsculos e minúsculos;
+ * O 'X' para números e letras, mas todas as letras serão maiúsculas (não sei porque);
+ * O 'P' para todo e qualquer caractere que possa ser impresso.
+ */
+ 

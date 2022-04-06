95850321f6fb7b38bdcf098b24dd7f250dbf75ff
Filipe Soares <58039037+Filipe-acht@users.noreply.github.com>
2022-04-06 09:46:21 -0300

    Update variavel_record.sql

--- Aprendendo/variavel_record.sql
+++ Aprendendo/variavel_record.sql
@@ -11,3 +11,18 @@ BEGIN
 	  FROM <tabela>.plsq_learning WHERE id =  1;
 	dbms_output.put_line(pessoa_record.id || ' - ' || pessoa_record.nome);
 END;
+/*
+   
+DECLARE 
+TYPE pessoa_record_type IS RECORD
+		(
+		id <tabela>.plsq_learning.id%TYPE,
+		nome <tabela>.plsq_learning.nome%type
+		);
+pessoa_record pessoa_record_type;
+BEGIN
+	SELECT id, nome INTO pessoa_record 
+	  FROM <tabela>.plsq_learning WHERE id =  1;
+	dbms_output.put_line(pessoa_record.id || ' - ' || pessoa_record.nome);
+END;
+*/
